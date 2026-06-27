998df675c8dec8ff20b9873a1c27d0e3fa604647
Roland <srolo2007@gmail.com>
2026-06-27 16:34:42 +0200

    make slot datas

--- scripts/grid_system/data/Slot.cs
+++ scripts/grid_system/data/Slot.cs
@@ -3,4 +3,11 @@ using System;
 
 public partial class Slot : Resource
 {
+	public Entity? EntityHere {get;set;};
+	public Vector2I Position {get; private set}
+	public Slot(Vector2I Position)
+	{
+		this.Position=Position;
+		this.EntityHere=null;
+	}
 }
