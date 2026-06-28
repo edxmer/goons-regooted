41b8a1af3239649060cc500b6ef28834653f0953
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-06-28 22:47:05 +0200

    Format document

--- scripts/grid_system/data/Map.cs
+++ scripts/grid_system/data/Map.cs
@@ -4,30 +4,30 @@ using System;
 using System.Collections.Generic;
 public partial class Map : Resource
 {
-	public Chunk?[,] MapChunks {get;private set;}
+	public Chunk?[,] MapChunks { get; private set; }
 	private Queue<Chunk?> ChunkQueue;
 	public Map()
 	{
-		MapChunks=new Chunk?[GlobalParameters.MAP_CHUNK_HEIGHT,GlobalParameters.MAP_CHUNK_WIDTH];
+		MapChunks = new Chunk?[GlobalParameters.MAP_CHUNK_HEIGHT, GlobalParameters.MAP_CHUNK_WIDTH];
 		ChunkQueue = new Queue<Chunk?>();
 		ChunkQueue.Enqueue(null);
-		for (int yc=0;yc<GlobalParameters.MAP_CHUNK_HEIGHT;yc++)
+		for (int yc = 0; yc < GlobalParameters.MAP_CHUNK_HEIGHT; yc++)
 		{
-			for (int xc=0;xc<GlobalParameters.MAP_CHUNK_WIDTH;xc++)
+			for (int xc = 0; xc < GlobalParameters.MAP_CHUNK_WIDTH; xc++)
 			{
-				MapChunks[yc,xc]=null;
+				MapChunks[yc, xc] = null;
 			}
 		}
 	}
 	private Chunk? QueueScrollOne()
 	{
-		Chunk? a=ChunkQueue.Dequeue();
+		Chunk? a = ChunkQueue.Dequeue();
 		ChunkQueue.Enqueue(a);
 		return a;
 	}
 	private void ScrollToFirst()
 	{
-		while(QueueScrollOne() is not null)
+		while (QueueScrollOne() is not null)
 		{
 			continue;
 		}
@@ -39,30 +39,30 @@ public partial class Map : Resource
 		(
 			(ChunkQueue.Peek() is not null) &&
 			 (
-				(ChunkQueue.Peek().TopLeftPos.Y<chunk.TopLeftPos.Y) 
-				|| 
-				(ChunkQueue.Peek().TopLeftPos.Y==chunk.TopLeftPos.Y && ChunkQueue.Peek().TopLeftPos.X<chunk.TopLeftPos.X)
+				(ChunkQueue.Peek().TopLeftPos.Y < chunk.TopLeftPos.Y)
+				||
+				(ChunkQueue.Peek().TopLeftPos.Y == chunk.TopLeftPos.Y && ChunkQueue.Peek().TopLeftPos.X < chunk.TopLeftPos.X)
 			 )
 		)
 		{
 			QueueScrollOne();
 		}
-		if ((ChunkQueue.Peek() is null) || !(ChunkQueue.Peek().TopLeftPos==chunk.TopLeftPos))
+		if ((ChunkQueue.Peek() is null) || !(ChunkQueue.Peek().TopLeftPos == chunk.TopLeftPos))
 		{
 			ChunkQueue.Enqueue(chunk);
 		}
 		ScrollToFirst();
 	}
-	
+
 	public Vector2I GetChunkCoordFromCoords(Vector2I Pos)
 	{
 		return new Vector2I(
-			Mathf.FloorToInt((float)Pos.X/GlobalParameters.CHUNK_SIZE),
-			Mathf.FloorToInt((float)Pos.Y/GlobalParameters.CHUNK_SIZE));
+			Mathf.FloorToInt((float)Pos.X / GlobalParameters.CHUNK_SIZE),
+			Mathf.FloorToInt((float)Pos.Y / GlobalParameters.CHUNK_SIZE));
 	}
 	public void LoadChunkEmptyBase(Vector2I ChunkCoord)
 	{
-		MapChunks[ChunkCoord.Y,ChunkCoord.X]=new Chunk(ChunkCoord*GlobalParameters.CHUNK_SIZE);
+		MapChunks[ChunkCoord.Y, ChunkCoord.X] = new Chunk(ChunkCoord * GlobalParameters.CHUNK_SIZE);
 	}
-	
+
 }
