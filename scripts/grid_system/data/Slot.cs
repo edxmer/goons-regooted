43cb6df4efdd4baa82b3be6d14592a792ea94f18
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-06-28 22:47:55 +0200

    Format document

--- scripts/grid_system/data/Slot.cs
+++ scripts/grid_system/data/Slot.cs
@@ -4,13 +4,13 @@ using System;
 
 public partial class Slot : Resource
 {
-	
-	public  Entity? EntityHere {get;set;} 
-	public Vector2I Position {get; private set;}
+
+	public Entity? EntityHere { get; set; }
+	public Vector2I Position { get; private set; }
 	public Slot(Vector2I Position)
 	{
-		this.Position=Position;
-		this.EntityHere=null;
+		this.Position = Position;
+		this.EntityHere = null;
 	}
 	public int GetWeight()
 	{
