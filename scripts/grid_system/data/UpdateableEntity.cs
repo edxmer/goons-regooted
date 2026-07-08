4b90e35f8585fbaba9c0fd14c2b9a4f15f512e3d
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-07-08 17:25:20 +0200

    Update constructor

--- scripts/grid_system/data/UpdateableEntity.cs
+++ scripts/grid_system/data/UpdateableEntity.cs
@@ -4,8 +4,8 @@ using System;
 public abstract partial class UpdateableEntity : Entity
 {
 	public virtual bool HasUpdateFunction => true;
-	public UpdateableEntity(Map map,Vector2I TopLeftPos):base(map,TopLeftPos)
-	{}
+
+	public UpdateableEntity(Vector2I TopLeftPos): base(TopLeftPos) {}
 	/*TickNumber is a number between 0-19*/
 	public abstract void OnUpdate(int TickNumber);
 }
