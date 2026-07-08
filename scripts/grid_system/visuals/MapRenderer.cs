6829354ac735d050591a60d58778f0c0ddd3efbf
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-07-08 20:07:24 +0200

    Make MapRenderer work with multi-grid entities

--- scripts/grid_system/visuals/MapRenderer.cs
+++ scripts/grid_system/visuals/MapRenderer.cs
@@ -15,8 +15,13 @@ public partial class MapRenderer : Node2D
 
 		// TODO: Spawn in test rock at (0, 0), should remove later
 		Global.Map.SetPosTo(
-			new TestStaticEntity(Vector2I.Zero) {Texture = GD.Load<Texture2D>("res://assets/sprites/items/rock/spr_item_rock.png")},
+			new TestStaticEntity(Vector2I.Zero) { Texture = GD.Load<Texture2D>("res://assets/sprites/items/rock/spr_item_rock.png") },
 			Vector2I.Zero
+		);
+
+		Global.Map.SetPosTo(
+			new TestStaticEntity(new Vector2I(1, 1)) { Texture = GD.Load<Texture2D>("res://assets/sprites/items/rock/spr_item_rock.png") },
+			new Vector2I(1, 1)
 		);
 	}
 
@@ -24,10 +29,10 @@ public partial class MapRenderer : Node2D
 	// TODO: We should also add animations when tiles move, but only after 
 	// the basic functionalities are complete.
 
-    public override void _Draw()
-    {
-        base._Draw();
-		
+	public override void _Draw()
+	{
+		base._Draw();
+
 		if (Global.Map is null || Global.PlayerCamera is null) return;
 
 		Rect2 cameraViewRect = Global.PlayerCamera.GetCameraViewRect();
@@ -35,12 +40,12 @@ public partial class MapRenderer : Node2D
 		Vector2 cameraBotRightCorner = cameraTopLeftCorner + cameraViewRect.Size;
 
 		Vector2I topLeftCoords = new(
-			Mathf.CeilToInt(cameraTopLeftCorner.X / (float)PixelsPerTile), 
+			Mathf.CeilToInt(cameraTopLeftCorner.X / (float)PixelsPerTile),
 			Mathf.CeilToInt(cameraTopLeftCorner.Y / (float)PixelsPerTile)
 		);
 
 		Vector2I botRightCoords = new(
-			Mathf.FloorToInt(cameraBotRightCorner.X / (float)PixelsPerTile), 
+			Mathf.FloorToInt(cameraBotRightCorner.X / (float)PixelsPerTile),
 			Mathf.FloorToInt(cameraBotRightCorner.Y / (float)PixelsPerTile)
 		);
 
@@ -49,12 +54,13 @@ public partial class MapRenderer : Node2D
 			{
 				Entity entity = Global.Map.GetEntityAtPos(new Vector2I(x, y));
 
+				var coords = new Vector2I(x, y);
 
-				if (entity is not null && entity.Texture is not null)
+				if (entity is not null && entity.Texture is not null && entity.TopLeftPosition == coords)
 				{
 					var rect = new Rect2(
-						new Vector2(x*PixelsPerTile, y*PixelsPerTile), 
-						new Vector2((x+1)*PixelsPerTile, (y+1)*PixelsPerTile)
+						new Vector2(x * PixelsPerTile, y * PixelsPerTile),
+						new Vector2((x + entity.GetWidth()) * PixelsPerTile, (y + entity.GetHeight()) * PixelsPerTile)
 					);
 
 					DrawTextureRect(entity.Texture, rect, tile: false);
@@ -62,5 +68,5 @@ public partial class MapRenderer : Node2D
 					GD.Print($"Drawing entity at position ({x}, {y}).");
 				}
 			}
-    }
+	}
 }
