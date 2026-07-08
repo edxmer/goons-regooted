4eb5284e8a5948f96f2a97cfd75331954a479e60
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-07-08 17:08:47 +0200

    Add Map and PlayerCamera instance variables

--- scripts/global/Global.cs
+++ scripts/global/Global.cs
@@ -3,7 +3,12 @@ using Godot;
 
 public static class Global
 {
+	/* --- MAP CONSTANTS --- */
 	public const int CHUNK_SIZE = 64;
 	public const int MAP_CHUNK_WIDTH = 100;
 	public const int MAP_CHUNK_HEIGHT = 100;
+	
+	/* --- INSTANCES --- */
+	public static Map Map { get; set; }
+	public static PlayerCamera PlayerCamera { get; set; }
 }
